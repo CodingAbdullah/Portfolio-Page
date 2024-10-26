0f77047061998569fdf019dda4a53030077df60b
CodingAbdullah <abdullahmd326@gmail.com>
2024-10-25 23:04:07 -0400

    Updating projects within Personal site

--- src/Components/utils/constants.ts
+++ src/Components/utils/constants.ts
@@ -9,32 +9,32 @@ type ProjectListSetup = {
 export const constants : ProjectListSetup = [
     { 
         name : 'Ethereum Wallet Dashboard', 
-        description : "A descriptive web app, detailing the history of a wallet's transactions including different ERC transactions. Powered by React, Node, Express", 
-        languages: ['HTML', 'CSS', 'Javascript', 'Shell'],
-        url: 'https://ethwdashboard.xyz'
+        description : "A descriptive web app, detailing the history of a wallet's transactions including different ERC transactions.", 
+        languages: ['HTML', 'CSS', 'React', 'Node', 'Express', 'AWS', 'Shell'],
+        url: 'https://ethereumdashboard.dev'
     },
     {
-        name : 'Ethereum Wallet Dashboard Test', 
-        description : "QA application which tests the features of the Dashboard application, using Selenium WebDriver and the Cucumber Framework (BDD)", 
-        languages: ['Java', 'Gherkin'],
-        url: 'https://github.com/CodingAbdullah/Ethereum-Wallet-Dashboard-Test'
+        name : 'Layer Two Dashboards', 
+        description : "Arbitrum, Optimism, and Polygon dashboards created using a minified version of the Ethereum dashboard above.", 
+        languages: ['HTML', 'CSS', 'React', 'Node', 'Express', 'AWS', 'Shell'],
+        url: 'https://polygondashboard.xyz'
     },
     {
-        name : 'Green Kart Web App',
-        description: "A simple online store allowing users to purchase items. Full Stack Web Application using Mongo, Express, React, Redux, and Node",
-        languages: ['HTML', 'CSS', 'Javascript'],
-        url: 'https://greenkartstore.com'
+        name : 'Medium Article Scraper Tool',
+        description: "Tool that allows users to extract Medium.com article information in the form of text and audio files.",
+        languages: ['HTML', 'CSS', 'React', 'Node', 'Express', 'OpenAI API', 'AWS'],
+        url: 'https://mediumdotcomscraper.pro'
     },
     {
         name: "DALLE 2's Bestie",
-        description: 'A website which allows users to sign up, search, and save AI generated images. Powered by React, Redux, Node, Express, Mongo, and Nodemailer',
-        languages: ['HTML', 'CSS', 'Javascript'],
+        description: 'Website that utilizes the OpenAI DALLE API to generate images from a text prompt.',
+        languages: ['HTML', 'CSS', 'React', 'Node', 'Express', 'OpenAI API', 'AWS'],
         url: 'https://dalle2sbestie.com'
     },
     {
-        name: "Personal Website",
-        description: "A site built dedicated to the life of me. The development journey, projects, hobbies, and just about everything else there is to know about web3 and the crypto space",
-        languages: ['HTML', 'CSS', 'Javascript'],
-        url: 'https://kingabdullah.codes'
+        name: "Next.js SAAS Deployer CI-CD Pipeline",
+        description: "Forked from the original SAAS starter template, CI/CD pipeline features added. Allowing users to freely build, test, and deploy software.",
+        languages: ['HTML', 'CSS', 'Next.js', 'Jenkins', 'Docker', 'Terraform', 'Shell', 'AWS'],
+        url: 'https://github.com/CodingAbdullah/next-saas-starter-ci-cd'
     }
 ];
